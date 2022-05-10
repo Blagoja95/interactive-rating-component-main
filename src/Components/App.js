349d3177d30fc4223f5ab54d94337e67029d4d3d
Blagoja95 <blagojevic.info@gmail.com>
2022-05-10 17:30:03 +0200

    component done

--- src/Components/App.js
+++ src/Components/App.js
@@ -3,9 +3,12 @@ import Header from "./Header";
 import Body from "./Body";
 import Rating from "./Rating";
 import Submit from "./Submit";
+import Message from "./Message";
 import "../sass/main.scss";
+
 const App = () => {
   const [rating, setRating] = useState(0);
+  const [message, setMessage] = useState(false);
 
   useEffect(() => {
     // console.log(rating);
@@ -18,18 +21,24 @@ const App = () => {
 
     const face = document.querySelector(".component");
     while (face.firstChild) face.removeChild(face.firstChild);
-    // console.log(face.firstChild);
+
     // console.log(rating);
+    // console.log(face.firstChild);
+
+    if (!face.firstChild) setMessage(true);
   };
 
-  return (
-    <main className="component">
-      <Header />
-      <Body />
-      <Rating handleResult={handleResult} />
-      <Submit handleSubmit={handleSubmit} />
-    </main>
-  );
+  if (!message)
+    return (
+      <main className="component">
+        <Header />
+        <Body />
+        <Rating handleResult={handleResult} />
+        <Submit handleSubmit={handleSubmit} />
+      </main>
+    );
+
+  if (message) return <Message rating={rating} />;
 };
 
 export default App;
--- src/Components/Message.js
+++ src/Components/Message.js
@@ -3,15 +3,18 @@ import messageSVG from "../images/illustration-thank-you.svg";
 
 const Message = ({ rating }) => {
   return (
-    <div className="message">
-      <img className="message__img" src="messageSVG" alt="success" />
+    <main className="component message">
+      <img className="message__img" src={messageSVG} alt="success" />
 
       <p className="paragraph message__paragraph--result">{`You selected ${rating} out of 5`}</p>
 
       <h1 className="title message__title">Thank you!</h1>
       <p className="paragraph message__paragraph">
-          We appreciate
+        We appreciate you taking time to give a rating. if you ever need more
+        support, don't hesitate to get in touch!
       </p>
-    </div>
+    </main>
   );
 };
+
+export default Message;
